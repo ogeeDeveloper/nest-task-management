73eaed2fba1f9972bce6a47cf2a1afed625886e4
ogeeDeveloper <omarogrant202@hotmail.com>
2022-09-11 13:21:27 -0500

    Created and invoked the taskService in task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
+import { TasksService } from './tasks.service';
 import { Controller } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    // Inject the service in our controller
+    constructor(private tasksService:TasksService){}
+}
