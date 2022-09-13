941c51af5c98eea6b012ff6e20c2022a9df4ae69
ogeeDeveloper <omarogrant202@hotmail.com>
2022-09-13 13:27:18 -0500

    Added Feature: to get a task by Id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
 import { CreateTaskDto } from './../dto/create-task.dto';
 import { TasksService } from './tasks.service';
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { Task } from './task.module';
 
 @Controller('tasks')
@@ -8,10 +8,17 @@ export class TasksController {
     // Inject the service in our controller
     constructor(private tasksService:TasksService){}
 
+    // http://localhost:3000/
     // Retrieve the tasks that was exposed by the TaskService
     @Get()
     getAllTasks(): Task[]{
         return this.tasksService.getAllTasks()
+    }
+
+    // http://localhost:3000/<someId>
+    @Get('/:id')
+    getTakById(@Param('id') id: string): Task{
+        return this.tasksService.getTakById(id)
     }
 
     @Post()
